e58bea92ba24c1443a6377db6cdd16c6930edaca
ShafigaBagirova <bagiroffa06@gmail.com>
2025-11-26 20:00:34 +0400

    modified commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
+Person person = new()
+{
+    Name = "Shafiga",
+    Surname = "Bagirova"
+};
+Manager manager = new()
+{ 
+    Name="Aysel",
+     Surname="Ehmedova"
+};
+
+Developer developer = new ()
+{
+    Name = "Gunel",
+    Surname = "Elyeva"
+};
+developer.ShowInfo();
+person.ShowInfo();
+manager.ShowInfo();
+
+
